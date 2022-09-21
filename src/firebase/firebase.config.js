ebed49b992b77bf508d40b875dfd98baaa485055
JSjags <jesseabuaja@hotmail.com>
2022-09-21 09:16:04 +0100

    Added environment variables.

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getStorage } from "firebase/storage";
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
+  authDomain: process.env.REACT_APP_AUTHDOMAIN,
+  projectId: process.env.REACT_APP_PROJECTID,
+  storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERIP,
+  appId: process.env.REACT_APP_APPID,
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const storage = getStorage(app);
+storage.maxUploadRetryTime = 6000;
