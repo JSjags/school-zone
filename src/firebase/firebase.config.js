61017b034f0a099c2ba1cfe96b932361569cb62c
JSjags <jesseabuaja@hotmail.com>
2022-09-21 01:04:16 +0100

    Added firebase file to production.

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getStorage } from "firebase/storage";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB_EDMkC6__iu9xBx7ePAmb_PFstqeFz0E",
+  authDomain: "schoolzone-project.firebaseapp.com",
+  projectId: "schoolzone-project",
+  storageBucket: "schoolzone-project.appspot.com",
+  messagingSenderId: "77743137094",
+  appId: "1:77743137094:web:6c0d3bd1f8cd2f77018547",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const storage = getStorage(app);
+storage.maxUploadRetryTime = 6000;
