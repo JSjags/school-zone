aff5bba259133d63cce6a9135a04e5295aa40e3d
JSjags <jesseabuaja@hotmail.com>
2022-09-22 10:27:00 +0100

    Replaced syncfusion license with environment variables.

--- src/index.js
+++ src/index.js
@@ -4,8 +4,11 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { store } from "./app/store";
 import { Provider } from "react-redux";
-
 import { BrowserRouter as Router } from "react-router-dom";
+import { registerLicense } from "@syncfusion/ej2-base";
+
+// Registering Syncfusion license key
+registerLicense(process.env.REACT_APP_SYNCFUSION_LICENSE_KEY);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
